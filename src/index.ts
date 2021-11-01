1c606231dae0e1c4c76b95fa594504b4cabb4795
Don Livingston <donlivingston@gmail.com>
2021-10-31 22:10:17 -0400

    adding webpack-dev-server and html-webpack-plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import * as _ from 'lodash';
 const dl = "DL Build Kit 2021"
+
+function component() {
+  const element = document.createElement('div');
+  
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+  return element;
+}
+
+document.body.appendChild(component());
+console.log(dl);
